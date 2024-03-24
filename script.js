d4bfcf87570c00fe10889e8d4c620b182cdc8571
Pietro Martins <marpietro2007@gmail.com>
2024-03-24 18:59:23 -0300

    nono commit do CSS e terceiro commit do JS

--- script.js
+++ script.js
@@ -28,10 +28,17 @@ function receberquantidadepessoas(evento) {
 
 const botoesgorjeta = document.querySelectorAll(".gorjeta input[type='button']")
 botoesgorjeta.forEach(botao => {
-    botao.addEventListener("click", receberporcentagem)
+    botao.addEventListener("click", receberporcentagembotao)
 })
 
-function receberporcentagem(evento) {
-    porcentagem = Number(evento.target.value) /100
-    console.log(evento.target.value)
+function receberporcentagembotao(evento) {
+    botoesgorjeta.forEach(botao => {
+        botao.classList.remove("botao-ativo")
+
+        if(botao.value === evento.target.value) {
+            botao.classList.add("botao-ativo")
+        }
+    })
+
+    porcentagem = parseFloat(evento.target.value) /100
 }
